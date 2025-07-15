8dcfe296874e00c5c68b1dd63cdb116f97d348b4
Rob Parrett <robparrett@gmail.com>
2025-07-15 10:54:30 -0700

    Fix TS errors

--- stores/BluetoothState.ts
+++ stores/BluetoothState.ts
@@ -42,7 +42,7 @@ export const useBluetoothState = create<BluetoothState>((set, get) => {
   return {
     // State
     bleManager,
-    bluetoothState: 'Unknown',
+    bluetoothState: State.Unknown,
     isScanning: false,
     devices: [],
     connectedDevice: null,
---
+++ types/themed.d.ts
@@ -0,0 +1,8 @@
+import '@rneui/themed';
+
+declare module '@rneui/themed' {
+  export interface Colors {
+    secondarySurface: string;
+    star: string;
+  }
+}
