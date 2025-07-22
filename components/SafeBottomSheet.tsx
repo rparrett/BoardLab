5bfd3981bc93c2665d6d5b905580e8e96ed6b883
Rob Parrett <robparrett@gmail.com>
2025-07-22 12:32:14 -0700

    Ensure text input is visible when keyboard appears

--- components/SafeBottomSheet.tsx
+++ components/SafeBottomSheet.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BottomSheet, makeStyles } from '@rn-vui/themed';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { useWindowDimensions } from 'react-native';
+import { useWindowDimensions, Keyboard } from 'react-native';
 
 interface SafeBottomSheetProps {
   isVisible: boolean;
@@ -18,6 +18,29 @@ export default function SafeBottomSheet({
 }: SafeBottomSheetProps) {
   const insets = useSafeAreaInsets();
   const { height } = useWindowDimensions();
+  const [keyboardHeight, setKeyboardHeight] = useState(0);
+
+  useEffect(() => {
+    const keyboardWillShowListener = Keyboard.addListener(
+      'keyboardWillShow',
+      e => {
+        setKeyboardHeight(e.endCoordinates.height);
+      },
+    );
+
+    const keyboardWillHideListener = Keyboard.addListener(
+      'keyboardWillHide',
+      () => {
+        setKeyboardHeight(0);
+      },
+    );
+
+    return () => {
+      keyboardWillShowListener.remove();
+      keyboardWillHideListener.remove();
+    };
+  }, []);
+
   const styles = useStyles({ maxHeight: height * maxHeightPercentage });
 
   return (
@@ -26,7 +49,14 @@ export default function SafeBottomSheet({
       onBackdropPress={onBackdropPress}
       scrollViewProps={{
         style: styles.container,
-        contentInset: { top: 0, left: 0, right: 0, bottom: insets.bottom },
+        contentInset: {
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: insets.bottom + keyboardHeight,
+        },
+        contentOffset:
+          keyboardHeight > 0 ? { x: 0, y: keyboardHeight } : { x: 0, y: 0 },
       }}
       // @ts-expect-error BottomSheet takes SafeAreaView's props as well, but its types are incorrect
       edges={{ bottom: 'off' }}
